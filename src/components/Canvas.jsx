0ce6381cc1545173d92acef7a90befcdb1dfa8d9
Zaxaromy <mistereyeszi101@gmail.com>
2023-10-06 23:37:58 +0200

    Canvas
    Signed-off-by: Zaxaromy <mistereyeszi101@gmail.com>

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -8,6 +8,29 @@ const Canvas = ({ socket }) => {
         const context = canvas.getContext('2d');
         let isDrawing = false;
 
+        socket.on('draw', (data) => {
+            // Extract drawing data (for example, x and y coordinates)
+            const { x, y } = data;
+
+            // Begin a new path and move to the specified coordinates
+            context.beginPath();
+
+            context.arc(x, y, 5, 0, Math.PI * 2)
+
+            // Set line style and color
+            context.lineWidth = 2;
+            context.strokeStyle = 'black';
+
+            // Stroke the path (draw the line)
+            context.stroke();
+
+            context.fill();
+
+            context.closePath();
+
+            console.log(data)
+        });
+
         canvas.addEventListener('mousedown', () => {
             isDrawing = true;
         });
@@ -18,10 +41,6 @@ const Canvas = ({ socket }) => {
             const rect = canvas.getBoundingClientRect();
             const x = e.clientX - rect.left;
             const y = e.clientY - rect.top;
-
-            // Draw on the canvas
-            context.lineTo(x, y);
-            context.stroke();
 
             // Send drawing data to the server
             socket.emit('draw', { x, y });
