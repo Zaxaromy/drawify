5eef96308cfa118867bf3811cf1930b2f0f41216
Zaxaromy <mistereyeszi101@gmail.com>
2023-10-19 00:36:31 +0200

    Changed Palette layout, added styling for underbar and fixed up canvas-page.styl
    Signed-off-by: Zaxaromy <mistereyeszi101@gmail.com>

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 
 function Palette() {
   return (
-    <div className="pallete">
-      <span className="black" />
-      <span className="dark-gray" />
-      <span className="gray" />
-      <span className="white" />
-      <span className="red" />
-      <span className="orange" />
-      <span className="yellow" />
-      <span className="green" />
-      <span className="blue" />
-      <span className="purple" />
-      <span className="brown" />
-      <span className="color-picker" />
+    <div className="palette-wrapper">
+      <div className="palette-top">
+        <span className="palette-color black" />
+        <span className="palette-color dark-gray" />
+        <span className="palette-color gray" />
+        <span className="palette-color white" />
+        <span className="palette-color red" />
+        <span className="palette-color orange" />
+      </div>
+      <div className="palette-bottom">
+        <span className="palette-color yellow" />
+        <span className="palette-color green" />
+        <span className="palette-color blue" />
+        <span className="palette-color pink" />
+        <span className="palette-color purple" />
+        <span className="palette-color brown" />
+        <span className="color-picker" />
+      </div>
     </div>
   );
 }
